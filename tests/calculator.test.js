0e251f992296f6014ab7223eed1ce08abc6eff19
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2024-04-06 00:16:21 +0530

    Add: tests to test divide and multiply function.

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,4 +1,4 @@
-import { add, subtract } from "../calculator";
+import { add, subtract, divide, multiply } from "../calculator";
 
 describe("calculator", () => {
     describe("add two numbers", () => {
@@ -63,10 +63,10 @@ describe("calculator", () => {
 
         describe("number is to big", () => {
             test("a = 9007199254740991 b = 3", () => {
-                expect(() => add(9007199254740996, 3)).toThrow("Inputs exceed the safe range (-9007199254740991 <= (a, b, a + b) <= 9007199254740991)!");
+                expect(() => add(9007199254740996, 3)).toThrow("Inputs exceed the safe range (-9007199254740991 <= (a, b, a + b, a - b, a / b, a * b) <= 9007199254740991)!");
             });
             test("a = -9007199254740991 b = -3", () => {
-                expect(() => add(-9007199254740996, -3)).toThrow("Inputs exceed the safe range (-9007199254740991 <= (a, b, a + b) <= 9007199254740991)!");
+                expect(() => add(-9007199254740996, -3)).toThrow("Inputs exceed the safe range (-9007199254740991 <= (a, b, a + b, a - b, a / b, a * b) <= 9007199254740991)!");
             });
         });
     });
@@ -138,21 +138,221 @@ describe("calculator", () => {
         describe("numbers is to big", () => {
             describe("one input is out of safe range", () => {
                 test("a = 9007199254740998 b = 4", () => {
-                    expect(() => subtract(9007199254740998, 4)).toThrow("Inputs exceed the safe range (-9007199254740991 <= (a, b, a - b) <= 9007199254740991)!");
+                    expect(() => subtract(9007199254740998, 4)).toThrow("Inputs exceed the safe range (-9007199254740991 <= (a, b, a + b, a - b, a / b, a * b) <= 9007199254740991)!");
                 });
                 test("a = 10 b = 9007199254740996", () => {
-                    expect(() => subtract(10, -9007199254740996)).toThrow("Inputs exceed the safe range (-9007199254740991 <= (a, b, a - b) <= 9007199254740991)!");
+                    expect(() => subtract(10, -9007199254740996)).toThrow("Inputs exceed the safe range (-9007199254740991 <= (a, b, a + b, a - b, a / b, a * b) <= 9007199254740991)!");
                 });
             }); 
 
             describe("both inputs are out of safe range", () => {
                 test("a = 9007199254740998 b = 9007199254740998", () => {
-                    expect(() => subtract(9007199254740998, 9007199254740994)).toThrow("Inputs exceed the safe range (-9007199254740991 <= (a, b, a - b) <= 9007199254740991)!");
+                    expect(() => subtract(9007199254740998, 9007199254740994)).toThrow("Inputs exceed the safe range (-9007199254740991 <= (a, b, a + b, a - b, a / b, a * b) <= 9007199254740991)!");
                 });
                 test("a = 10 b = 9007199254740996", () => {
-                    expect(() => subtract(9007199254740992, -9007199254740996)).toThrow("Inputs exceed the safe range (-9007199254740991 <= (a, b, a - b) <= 9007199254740991)!");
+                    expect(() => subtract(9007199254740992, -9007199254740996)).toThrow("Inputs exceed the safe range (-9007199254740991 <= (a, b, a + b, a - b, a / b, a * b) <= 9007199254740991)!");
                 });
             }); 
         })
     });
+
+    describe("divide two number", () => {
+        describe("integer", () => {
+            test("a = 2 b = 2", () => {
+                expect(divide(2, 2)).toEqual(1);
+            });
+            test("a = -4 b = 2", () => {
+                expect(divide(-4, 2)).toEqual(-2)
+            });
+            test("a = 6 b = 3", () => {
+                expect(divide(6, 3)).toEqual(2);
+            });
+            test("a = 0 b = 0", () => {
+                expect(divide(0, 0)).toEqual(NaN);
+            });
+            test("a = 1 b = 0", () => {
+                expect(divide(1, 0)).toEqual(Infinity);
+            });
+            test("a = 0 b = 4", () => {
+                expect(divide(0, 4)).toEqual(0);
+            });
+        }); 
+
+        describe("floating point number", () => {
+            test("a = 1 b = 2", () => {
+                expect(divide(1, 2)).toBeCloseTo(0.5);
+            });
+            test("a = 0.7 b = 0.5", () => {
+                expect(divide(0.7, 0.5)).toBeCloseTo(1.4);
+            });
+            test("a = -0.7 b = 0.5", () => {
+                expect(divide(-0.7, 0.5)).toBeCloseTo(-1.4);
+            });
+            test("a = -0.3 b = -0.5", () => {
+                expect(divide(-0.3, -0.5)).toBeCloseTo(0.6);
+            });
+        });
+
+        describe("invalid inputs", () => {
+            test("empty", () => {
+                expect(() => divide()).toThrow("Inputs are not valid numbers!");
+            });
+
+            describe("one input is invalid", () => {
+                test("a = '2' b = 3", () => {
+                    expect(() => divide("1", 3)).toThrow("Inputs are not valid numbers!");
+                });
+                test("a = null b = 4", () => {
+                    expect(() => divide(null, 4)).toThrow("Inputs are not valid numbers!")
+                });
+                test("a = 6 b = NaN", () => {
+                    expect(() => divide(6, NaN)).toThrow("Inputs are not valid numbers!")
+                });
+                test("a = undefined b = 4", () => {
+                    expect(() => divide(undefined, 4)).toThrow("Inputs are not valid numbers!")
+                });
+                test("a = Infinite b = 4", () => {
+                    expect(() => divide(Infinity  , 4)).toThrow("Inputs are not valid numbers!")
+                });
+            });
+
+            describe("both inputs are invalid", () => {
+                test("a = '2' b = '3'", () => {
+                    expect(() => divide("1", "3")).toThrow("Inputs are not valid numbers!");
+                });
+                test("a = null b = undefined", () => {
+                    expect(() => divide(null, undefined)).toThrow("Inputs are not valid numbers!")
+                });
+                test("a = NaN b = NaN", () => {
+                    expect(() => divide(NaN, NaN)).toThrow("Inputs are not valid numbers!")
+                });
+                test("a = undefined b = -Infinity", () => {
+                    expect(() => divide(undefined, -Infinity)).toThrow("Inputs are not valid numbers!")
+                });
+                test("a = Infinite b = -Infinity", () => {
+                    expect(() => divide(Infinity  , -Infinity)).toThrow("Inputs are not valid numbers!")
+                });
+            });
+        });
+
+        describe("numbers is to big", () => {
+            describe("one input is out of safe range", () => {
+                test("a = 9007199254740998 b = 4", () => {
+                    expect(() => divide(9007199254740998, 4)).toThrow("Inputs exceed the safe range (-9007199254740991 <= (a, b, a + b, a - b, a / b, a * b) <= 9007199254740991)!");
+                });
+                test("a = 10 b = 9007199254740996", () => {
+                    expect(() => divide(10, -9007199254740996)).toThrow("Inputs exceed the safe range (-9007199254740991 <= (a, b, a + b, a - b, a / b, a * b) <= 9007199254740991)!");
+                });
+            }); 
+
+            describe("both inputs are out of safe range", () => {
+                test("a = 9007199254740998 b = 9007199254740998", () => {
+                    expect(() => divide(9007199254740998, 9007199254740994)).toThrow("Inputs exceed the safe range (-9007199254740991 <= (a, b, a + b, a - b, a / b, a * b) <= 9007199254740991)!");
+                });
+                test("a = 10 b = 9007199254740996", () => {
+                    expect(() => divide(9007199254740992, -9007199254740996)).toThrow("Inputs exceed the safe range (-9007199254740991 <= (a, b, a + b, a - b, a / b, a * b) <= 9007199254740991)!");
+                });
+            }); 
+        })
+    });
+
+    describe("multipy two number", () => {
+        describe("integer", () => {
+            test("a = 2 b = 2", () => {
+                expect(multiply(2, 2)).toEqual(4);
+            });
+            test("a = -4 b = 2", () => {
+                expect(multiply(-4, 2)).toEqual(-8)
+            });
+            test("a = 6 b = 3", () => {
+                expect(multiply(6, 3)).toEqual(18);
+            });
+            test("a = 0 b = 0", () => {
+                expect(multiply(0, 0)).toEqual(0);
+            });
+            test("a = 1 b = 0", () => {
+                expect(multiply(1, 0)).toEqual(0);
+            });
+            test("a = 0 b = 4", () => {
+                expect(multiply(-8, -4)).toEqual(32);
+            });
+        });
+
+        describe("floating point number", () => {
+            test("a = 1 b = 2", () => {
+                expect(multiply(1.2, 2)).toBeCloseTo(2.4);
+            });
+            test("a = 0.7 b = 0.5", () => {
+                expect(multiply(0.7, 0.5)).toBeCloseTo(0.35);
+            });
+            test("a = -0.7 b = 0.5", () => {
+                expect(multiply(-0.7, 0.5)).toBeCloseTo(-0.35);
+            });
+            test("a = -0.3 b = -0.5", () => {
+                expect(multiply(-0.3, -0.5)).toBeCloseTo(0.15);
+            });
+        });
+
+        describe("invalid inputs", () => {
+            test("empty", () => {
+                expect(() => multiply()).toThrow("Inputs are not valid numbers!");
+            });
+
+            describe("one input is invalid", () => {
+                test("a = '2' b = 3", () => {
+                    expect(() => multiply("1", 3)).toThrow("Inputs are not valid numbers!");
+                });
+                test("a = null b = 4", () => {
+                    expect(() => multiply(null, 4)).toThrow("Inputs are not valid numbers!")
+                });
+                test("a = 6 b = NaN", () => {
+                    expect(() => multiply(6, NaN)).toThrow("Inputs are not valid numbers!")
+                });
+                test("a = undefined b = 4", () => {
+                    expect(() => multiply(undefined, 4)).toThrow("Inputs are not valid numbers!")
+                });
+                test("a = Infinite b = 4", () => {
+                    expect(() => multiply(Infinity  , 4)).toThrow("Inputs are not valid numbers!")
+                });
+            });
+
+            describe("both inputs are invalid", () => {
+                test("a = '2' b = '3'", () => {
+                    expect(() => multiply("1", "3")).toThrow("Inputs are not valid numbers!");
+                });
+                test("a = null b = undefined", () => {
+                    expect(() => multiply(null, undefined)).toThrow("Inputs are not valid numbers!")
+                });
+                test("a = NaN b = NaN", () => {
+                    expect(() => multiply(NaN, NaN)).toThrow("Inputs are not valid numbers!")
+                });
+                test("a = undefined b = -Infinity", () => {
+                    expect(() => multiply(undefined, -Infinity)).toThrow("Inputs are not valid numbers!")
+                });
+                test("a = Infinite b = -Infinity", () => {
+                    expect(() => multiply(Infinity  , -Infinity)).toThrow("Inputs are not valid numbers!")
+                });
+            });
+        });
+
+        describe("numbers is to big", () => {
+            describe("one input is out of safe range", () => {
+                test("a = 9007199254740998 b = 4", () => {
+                    expect(() => multiply(9007199254740998, 4)).toThrow("Inputs exceed the safe range (-9007199254740991 <= (a, b, a + b, a - b, a / b, a * b) <= 9007199254740991)!");
+                });
+                test("a = 10 b = 9007199254740996", () => {
+                    expect(() => multiply(10, -9007199254740996)).toThrow("Inputs exceed the safe range (-9007199254740991 <= (a, b, a + b, a - b, a / b, a * b) <= 9007199254740991)!");
+                });
+            }); 
+
+            describe("both inputs are out of safe range", () => {
+                test("a = 9007199254740998 b = 9007199254740998", () => {
+                    expect(() => multiply(9007199254740998, 9007199254740994)).toThrow("Inputs exceed the safe range (-9007199254740991 <= (a, b, a + b, a - b, a / b, a * b) <= 9007199254740991)!");
+                });
+                test("a = 10 b = 9007199254740996", () => {
+                    expect(() => multiply(9007199254740992, -9007199254740996)).toThrow("Inputs exceed the safe range (-9007199254740991 <= (a, b, a + b, a - b, a / b, a * b) <= 9007199254740991)!");
+                });
+            }); 
+        })
+    })
 });

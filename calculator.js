a440beb35b23b463156a8784e806b456d88fa1d5
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2024-04-06 00:02:58 +0530

    Separate the validation code into its own function.

--- calculator.js
+++ calculator.js
@@ -1,4 +1,4 @@
-export function add(a, b) {
+function isVaild(a, b) {
     if(
         (typeof a !== "number" || typeof b !== "number") || 
         (isNaN(a) || isNaN(b)) || !isFinite(a) || !isFinite(b)
@@ -13,25 +13,14 @@ export function add(a, b) {
     ) {
         throw new RangeError("Inputs exceed the safe range (-9007199254740991 <= (a, b, a + b) <= 9007199254740991)!");
     }
-    return a + b;
+    return true;
+}
+
+export function add(a, b) {
+    if(isVaild(a, b)) return a + b;
 }
 
 export function subtract(a, b) {
-    if(
-        (typeof a !== "number" || typeof b !== "number") || 
-        (isNaN(a) || isNaN(b)) ||
-        (!isFinite(a) || !isFinite(b))
-    ) {
-        throw new TypeError("Inputs are not valid numbers!");
-    }    
-    if(
-        (a > Number.MAX_SAFE_INTEGER || b > Number.MAX_SAFE_INTEGER) ||
-        (a < Number.MIN_SAFE_INTEGER || b < Number.MIN_SAFE_INTEGER) ||
-        a - b > Number.MAX_SAFE_INTEGER ||
-        a - b < Number.MIN_SAFE_INTEGER
-    ) {
-        throw new RangeError("Inputs exceed the safe range (-9007199254740991 <= (a, b, a - b) <= 9007199254740991)!");
-    }
-    return a - b; 
+    if(isVaild(a, b)) return a - b;
 } 
 

942da9f67a28021a89c51cc83ff9c1b73019b879
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2024-04-07 01:57:16 +0530

    update: use for of loop.

--- analyze-array.js
+++ analyze-array.js
@@ -6,20 +6,20 @@ export default function analyzeArray(arr) {
         throw new Error("input array is empty.");
     }
     let sum = 0;
-    let _min = arr[0];
-    let _max = arr[0];
-    for(let i = 0; i < arr.length; i++) {
-        if(typeof arr[i] !== "number" || isNaN(arr[i])){
+    let min = arr[0];
+    let max = arr[0];
+    for(const i of arr) {
+        if(typeof i !== "number" || isNaN(i)){
             throw new TypeError("there are non-numeric elements in the array.");
         } 
-        _min = arr[i] < _min ? arr[i] : _min;
-        _max = arr[i] > _max ? arr[i] : _max;
-        sum += arr[i];
+        min = i < min ? i : min;
+        max = i > max ? i : max;
+        sum += i;
     }
     return {
         average: sum / arr.length,
-        min: _min,
-        max: _max,
+        min,
+        max,
         length: arr.length
     }
 }
